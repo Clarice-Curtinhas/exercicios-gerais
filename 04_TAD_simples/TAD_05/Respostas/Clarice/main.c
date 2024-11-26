d0c5e287e16207d97358fd69428e84a302de4012
Clarice-Curtinhas <claricesc4@gmail.com>
2024-11-25 22:56:08 -0300

    não passou no valgrind

--- 04_TAD_simples/TAD_05/Respostas/Clarice/main.c
+++ 04_TAD_simples/TAD_05/Respostas/Clarice/main.c
@@ -6,38 +6,22 @@
 
 int main(){
     char nome[20], CPF[15];
-    int maxContas, operacao = -1, cadastros = 0;
+    int maxContas, operacao = 5, cadastros = 0;
     int nomeTam, CPFTam, numero, count;
     int numConta, achaConta;
     float valor;
 
     scanf("%d\n", &maxContas);
+
     tUsuario user[maxContas];
     tConta conta[maxContas];
+
     
     while(operacao != 0){
+
         scanf("%d\n", &operacao);
 
-        if(operacao == 3 && cadastros < maxContas){
-            for(nomeTam = 0; nome[nomeTam-1] != ' '; nomeTam++){
-                scanf("%c", &nome[nomeTam]);
-            }
-            nome[nomeTam] = '\0';
-
-            for(CPFTam = 0; CPF[CPFTam-1] != ' '; CPFTam++){
-                scanf("%c", &CPF[CPFTam]);
-            }
-            CPF[CPFTam] = '\0';
-
-            user[cadastros] = CriaUsuario(nome, CPF);
-
-            scanf("%d\n", &numero);
-
-            conta[cadastros] = CriaConta(numero, user[cadastros]);
-            cadastros++;
-        }
-
-        else if(operacao == 1){
+        if(operacao == 1){
             scanf("%d %f\n", &numConta, &valor);
 
             for(achaConta = 0; achaConta < cadastros; achaConta++){
@@ -61,6 +45,28 @@ int main(){
             }
         }
 
+        else if(operacao == 3 && cadastros < maxContas){
+
+            for(nomeTam = 0; nome[nomeTam-1] != ' '; ){
+                scanf("%c", &nome[nomeTam]);
+                nomeTam++;
+            }
+            nome[nomeTam] = '\0';
+
+            for(CPFTam = 0; CPF[CPFTam-1] != ' '; ){
+                scanf("%c", &CPF[CPFTam]);
+                CPFTam++;
+            }
+            CPF[CPFTam] = '\0';
+
+            user[cadastros] = CriaUsuario(nome, CPF);
+
+            scanf("%d\n", &numero);
+
+            conta[cadastros] = CriaConta(numero, user[cadastros]);
+            cadastros++;
+        }
+
         else if(operacao == 4){
             printf("===| Imprimindo Relatorio |===\n");
 
@@ -68,7 +74,6 @@ int main(){
                 ImprimeConta(conta[count]);
             }
         }
-
     }
 
     return 0;
--- 04_TAD_simples/TAD_05/Resultados/Clarice/main/main.c
+++ 04_TAD_simples/TAD_05/Resultados/Clarice/main/main.c
@@ -6,40 +6,21 @@
 
 int main(){
     char nome[20], CPF[15];
+    int maxContas, operacao = 5, cadastros = 0;
     int nomeTam, CPFTam, numero, count;
-    float valor;
-    int maxContas, operacao = -1, cadastros = 0;
     int numConta, achaConta;
+    float valor;
 
     scanf("%d\n", &maxContas);
+
     tUsuario user[maxContas];
     tConta conta[maxContas];
     
     while(operacao != 0){
+
         scanf("%d\n", &operacao);
 
-        if(operacao == 3 && cadastros < maxContas){
-            for(nomeTam = 0; nome[nomeTam-1] != ' '; ){
-                scanf("%c", &nome[nomeTam]);
-                nomeTam++;
-            }
-            nome[nomeTam] = '\0';
-
-            for(CPFTam = 0; CPF[CPFTam-1] != ' '; ){
-                scanf("%c", &CPF[CPFTam]);
-                CPFTam++;
-            }
-            CPF[CPFTam] = '\0';
-
-            user[cadastros] = CriaUsuario(nome, CPF);
-
-            scanf("%d\n", &numero);
-
-            conta[cadastros] = CriaConta(numero, user[cadastros]);
-            cadastros++;
-        }
-
-        else if(operacao == 1){
+        if(operacao == 1){
             scanf("%d %f\n", &numConta, &valor);
 
             for(achaConta = 0; achaConta < cadastros; achaConta++){
@@ -61,6 +42,28 @@ int main(){
                     break;
                 }
             }
+        }
+
+        else if(operacao == 3 && cadastros < maxContas){
+
+            for(nomeTam = 0; nome[nomeTam-1] != ' '; ){
+                scanf("%c", &nome[nomeTam]);
+                nomeTam++;
+            }
+            nome[nomeTam] = '\0';
+
+            for(CPFTam = 0; CPF[CPFTam-1] != ' '; ){
+                scanf("%c", &CPF[CPFTam]);
+                CPFTam++;
+            }
+            CPF[CPFTam] = '\0';
+
+            user[cadastros] = CriaUsuario(nome, CPF);
+
+            scanf("%d\n", &numero);
+
+            conta[cadastros] = CriaConta(numero, user[cadastros]);
+            cadastros++;
         }
 
         else if(operacao == 4){
